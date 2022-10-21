1a04d40e47674c70ee7b2ac41db0ceebc25e75f8
Ahmad Madany <ahmad.madany.01@gmail.com>
2022-10-21 23:58:36 +0300

    Make initial app design

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    es2021: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'no-underscore-dangle': ['off'],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-use-before-define': ['error', { functions: false }],
+    'consistent-return': 'off',
+    'no-shadow': 'off',
+    camelcase: 'off',
+    'no-multi-assign': ['error', { ignoreNonDeclaration: true }],
+    strict: 'off',
+    'lines-between-class-members': 'off',
+  },
+}
---
+++ initial-design/js/utils.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// eslint-disable-next-line prettier/prettier
+;(() => {
+  const flashMsgSection = document.querySelector('.section--flash-msgs')
+  if (!flashMsgSection) return
+  const flashMsgList = flashMsgSection.querySelector('ul')
+  const closeFlashMsgBtns = new Set(flashMsgList.querySelectorAll('.flash-msg .close-flash'))
+
+  function removeFlash(e) {
+    const { target } = e
+
+    const flashMsgEl = target.closest('.flash-msg')
+    flashMsgEl.remove()
+    closeFlashMsgBtns.delete(target)
+
+    if (closeFlashMsgBtns.size) return
+
+    flashMsgSection.remove()
+  }
+
+  closeFlashMsgBtns.forEach(btn => {
+    btn.addEventListener('click', removeFlash)
+  })
+})()
